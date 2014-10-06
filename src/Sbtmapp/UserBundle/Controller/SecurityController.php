46c3cbc4ba7f93b3b9ff85729ee9369b9326a7c6
Ian Phillips <ian.phillips@comverse.com>
2014-10-06 17:16:36 +0200

    Over-riding FOS SecurityController

---
+++ src/Sbtmapp/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,14 @@
+<?php
+
+// src/Acme/UserBundle/Controller/SecurityController.php
+
+namespace Sbtmapp\UserBundle\Controller;
+
+
+
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+
+class SecurityController extends BaseController
+{
+    
+}
