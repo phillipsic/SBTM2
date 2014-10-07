048a99b5aa1484c70d8333501fb36b8031d76e10
Ian Phillips <ian.phillips@comverse.com>
2014-10-07 18:16:29 +0200

    Corrected fixture files

--- src/Sbtmapp/SbtmBundle/DataFixtures/ORM/ProjectFixtures.php
+++ src/Sbtmapp/SbtmBundle/DataFixtures/ORM/ProjectFixtures.php
@@ -13,28 +13,28 @@ class ProjectFixtures extends AbstractFixture implements OrderedFixtureInterface
 
     public function load(ObjectManager $manager) {
         $project1 = new Project();
-        $project1->project_name('A testing project');
-        $project1->project_start_date('01/01/2015');
-        $project1->project_end_date('01/03/2015');
-        $project1->project_completed(False);
+        $project1->setProjectName('A testing project');
+        $project1->setProjectStartDate(new \DateTime());
+        $project1->setProjectEndDate(new \DateTime());
+        $project1->setProjectCompleted(False);
         $project1->setCreated(new \DateTime());
         $project1->setUpdated($project1->getCreated());
         $manager->persist($project1);
 
         $project2 = new Project();
-        $project2->project_name('Another testing project');
-        $project2->project_start_date('01/03/2015');
-        $project2->project_end_date('01/04/2015');
-        $project2->project_completed(False);
+        $project2->setProjectName('Another testing project');
+        $project2->setProjectStartDate(new \DateTime());
+        $project2->setProjectEndDate(new \DateTime());
+        $project2->setProjectCompleted(False);
         $project2->setCreated(new \DateTime());
         $project2->setUpdated($project2->getCreated());
         $manager->persist($project2);
 
         $project3 = new Project();
-        $project3->project_name('Another completed testing project');
-        $project3->project_start_date('01/03/2014');
-        $project3->project_end_date('01/04/2014');
-        $project3->project_completed(TRUE);
+        $project3->setProjectName('Another completed testing project');
+        $project3->setProjectStartDate(new \DateTime());
+        $project3->setProjectEndDate(new \DateTime());
+        $project3->setProjectCompleted(TRUE);
         $project3->setCreated(new \DateTime());
         $project3->setUpdated($project3->getCreated());
         $manager->persist($project3);
--- src/Sbtmapp/SbtmBundle/DataFixtures/ORM/SessionFixtures.php
+++ src/Sbtmapp/SbtmBundle/DataFixtures/ORM/SessionFixtures.php
@@ -14,24 +14,24 @@ class SessionFixtures extends AbstractFixture implements OrderedFixtureInterface
     public function load(ObjectManager $manager) {
         $session1 = new Session();
         $session1->setSessionName('A testing session');
-        $session1->setStatusId('1');
+        $session1->setStatusId($manager->merge($this->getReference('status-1')));
         $session1->setCharter('Session Charter');
         $session1->setAreas('Areas covered by the sesion.');
         $session1->setTestNotes('Notes to help the tester');
         $session1->setReady(true);
-        $session1->setProjectId('2');
+        $session1->setProjectId($manager->merge($this->getReference('project-1')));
         $session1->setCreated(new \DateTime());
         $session1->setUpdated($session1->getCreated());
         $manager->persist($session1);
 
         $session2 = new Session();
         $session2->setSessionName('A testing session');
-        $session2->setStatusId('1');
+        $session2->setStatusId($manager->merge($this->getReference('status-1')));
         $session2->setCharter('Session Charter');
         $session2->setAreas('Areas covered by the sesion.');
         $session2->setTestNotes('Notes to help the tester');
         $session2->setReady(true);
-        $session2->setProjectId('1');
+        $session2->setProjectId($manager->merge($this->getReference('project-1')));
         $session2->setCreated(new \DateTime());
         $session2->setUpdated($session2->getCreated());
         $manager->persist($session2);
@@ -40,12 +40,12 @@ class SessionFixtures extends AbstractFixture implements OrderedFixtureInterface
 
         $session3 = new Session();
         $session3->setSessionName('A testing session');
-        $session3->setStatusId('1');
+        $session3->setStatusId($manager->merge($this->getReference('status-1')));
         $session3->setCharter('Session Charter');
         $session3->setAreas('Areas covered by the sesion.');
         $session3->setTestNotes('Notes to help the tester');
         $session3->setReady(true);
-        $session3->setProjectId('1');
+        $session3->setProjectId($manager->merge($this->getReference('project-1')));
         $session3->setCreated(new \DateTime());
         $session3->setUpdated($session3->getCreated());
         $manager->persist($session3);
@@ -53,12 +53,12 @@ class SessionFixtures extends AbstractFixture implements OrderedFixtureInterface
 
         $session4 = new Session();
         $session4->setSessionName('A testing session');
-        $session4->setStatusId('1');
+        $session4->setStatusId($manager->merge($this->getReference('status-1')));
         $session4->setCharter('Session Charter');
         $session4->setAreas('Areas covered by the sesion.');
         $session4->setTestNotes('Notes to help the tester');
         $session4->setReady(true);
-        $session4->setProjectId('2');
+        $session4->setProjectId($manager->merge($this->getReference('project-2')));
         $session4->setCreated(new \DateTime());
         $session4->setUpdated($session4->getCreated());
         $manager->persist($session4);
@@ -67,12 +67,12 @@ class SessionFixtures extends AbstractFixture implements OrderedFixtureInterface
 
         $session5 = new Session();
         $session5->setSessionName('A testing session');
-        $session5->setStatusId('1');
+        $session5->setStatusId($manager->merge($this->getReference('status-1')));
         $session5->setCharter('Session Charter');
         $session5->setAreas('Areas covered by the sesion.');
         $session5->setTestNotes('Notes to help the tester');
         $session5->setReady(true);
-        $session5->setProjectId('1');
+        $session5->setProjectId($manager->merge($this->getReference('project-3')));
         $session5->setCreated(new \DateTime());
         $session5->setUpdated($session5->getCreated());
         $manager->persist($session5);
@@ -80,12 +80,12 @@ class SessionFixtures extends AbstractFixture implements OrderedFixtureInterface
 
         $session6 = new Session();
         $session6->setSessionName('A testing session');
-        $session6->setStatusId('1');
+        $session6->setStatusId($manager->merge($this->getReference('status-1')));
         $session6->setCharter('Session Charter');
         $session6->setAreas('Areas covered by the sesion.');
         $session6->setTestNotes('Notes to help the tester');
         $session6->setReady(true);
-        $session6->setProjectId('2');
+        $session6->setProjectId($manager->merge($this->getReference('project-2')));
         $session6->setCreated(new \DateTime());
         $session6->setUpdated($session6->getCreated());
         $manager->persist($session6);
@@ -94,12 +94,12 @@ class SessionFixtures extends AbstractFixture implements OrderedFixtureInterface
 
         $session7 = new Session();
         $session7->setSessionName('A testing session');
-        $session7->setStatusId('1');
+        $session7->setStatusId($manager->merge($this->getReference('status-1')));
         $session7->setCharter('Session Charter');
         $session7->setAreas('Areas covered by the sesion.');
         $session7->setTestNotes('Notes to help the tester');
         $session7->setReady(true);
-        $session7->setProjectId('1');
+        $session7->setProjectId($manager->merge($this->getReference('project-3')));
         $session7->setCreated(new \DateTime());
         $session7->setUpdated($session7->getCreated());
         $manager->persist($session7);
